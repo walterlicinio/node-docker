6c207b0c326cccb73b8db0f07da596ef10c70357
walterlicinio <licinio.laticinio@gmail.com>
2021-05-21 13:49:49 -0300

    User Routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,9 @@ const bcrypt = require('bcryptjs')
 
 exports.signUp = async (req,res,next) => {
   const {username, password} = req.body
-  const hashpassword = await bcrypt.hash(password, 12)
 
   try {
+    const hashpassword = await bcrypt.hash(password, 12)
     const newUser = await User.create({username, password:hashpassword})
     res.status(201).json({
       status : 'success',
@@ -18,5 +18,32 @@ exports.signUp = async (req,res,next) => {
     res.status(400).json({
       status: 'fail'
     })
+  }
+}
+
+exports.login = async (req,res,next) => {
+  const {username, password} = req.body
+  try {
+    const user = await User.findOne({username})
+    if (!user) { return res.status(400).json({status:'fail', message:'User not found'})}
+    
+    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+    if (passwordIsCorrect) {
+      res.status(200).json({
+        status:'success',
+        message:'User logged in'
+      })
+    } else {
+      res.status(400).json({
+        status:'fail',
+        message:'Incorrect username or password'
+      })
+    }
+
+  } catch (e) {
+    res.status(400).json({
+      status:'fail'
+    })
+    console.log(e)
   }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.post("/signup", userController.signUp)
-
+router.post("/login", userController.login)
 module.exports = router
